977bb97923bb1daeb40bd5cc0cff2081746d11ce
Hector <hectorbavio@gmail.com>
2012-10-30 17:09:05 -0300

    - Define reference transaction response methods

--- lib/paypal/recurring/response/reference_transaction.rb
+++ lib/paypal/recurring/response/reference_transaction.rb
@@ -2,6 +2,10 @@ module PayPal
   module Recurring
     module Response
       class ReferenceTransaction < Base
+        mapping(:reference_id => :REFERENCEID,
+                :description  => :DESC,
+                :amount       => :AMT,
+                :currency     => :CURRENCYCODE)
       end
     end
   end
