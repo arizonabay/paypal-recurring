7c4f20b03e821e65196df7dab10dd1db5ffb6a19
Hector <hectorbavio@gmail.com>
2012-11-02 12:37:39 -0300

    - Add more mapping methods

--- lib/paypal/recurring/response/billing_agreement.rb
+++ lib/paypal/recurring/response/billing_agreement.rb
@@ -2,6 +2,22 @@ module PayPal
   module Recurring
     module Response
       class BillingAgreement < Base
+
+        mapping(
+          :status       => :CHECKOUTSTATUS,
+          :email        => :EMAIL,
+          :email        => :EMAIL,
+          :payer_id     => :PAYERID,
+          :payer_status => :PAYERSTATUS,
+          :first_name   => :FIRSTNAME,
+          :last_name    => :LASTNAME,
+          :country      => :COUNTRYCODE,
+          :currency     => :CURRENCYCODE,
+          :amount       => :AMT,
+          :description  => :DESC,
+          :ipn_url      => :NOTIFYURL
+        )
+        
         def confirmation_url
           "#{PayPal::Recurring.site_endpoint}?cmd=_customer-billing-agreement&token=#{token}"
         end
--- lib/paypal/recurring/response/reference_transaction.rb
+++ lib/paypal/recurring/response/reference_transaction.rb
@@ -2,10 +2,15 @@ module PayPal
   module Recurring
     module Response
       class ReferenceTransaction < Base
-        mapping(:reference_id => :REFERENCEID,
-                :description  => :DESC,
-                :amount       => :AMT,
-                :currency     => :CURRENCYCODE)
+        mapping(:reference_id     => :BILLINGAGREEMENTID,
+                :description      => :DESC,
+                :amount           => :AMT,
+                :currency         => :CURRENCYCODE,
+                :transaction_id   => :TRANSACTIONID,
+                :fee_amt          => :FEEAMT,
+                :payment_status   => :PAYMENTSTATUS,
+                :payment_type     => :PAYMENTTYPE,
+                :transaction_type => :TRANSACTIONTYPE)
       end
     end
   end
